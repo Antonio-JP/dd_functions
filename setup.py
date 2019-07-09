19b2a95ec717730a3a1dbe13093c10dc65b89870
Antonio Jimenez Pastor <antonio@ebook.dk-compmath.jku.at>
2019-07-09 16:23:08 +0200

    Modified setup.py according the sage_sample project.
    Added a GNUv3 LICENSE file

--- setup.py
+++ setup.py
@@ -1,12 +1,50 @@
+## -*- encoding: utf-8 -*-
+import os
+import sys
+from setuptools import setup
+from codecs import open # To open the README file with proper encoding
+from setuptools.command.test import test as TestCommand # for tests
 
-from distutils.core import setup
+# Get information from separate files (README, VERSION)
+def readfile(filename):
+    with open(filename,  encoding='utf-8') as f:
+        return f.read()
+    
+# For the tests
+class SageTest(TestCommand):
+    def run_tests(self):
+        errno = os.system("sage -t --force-lib sage_sample")
+        if errno != 0:
+            sys.exit(1)
 
 setup(
-    name = "ajpastor",
-    version = "0.5",
+    name = "dd_functions",
+    version = readfile("VERSION").strip(), # the VERSION file is shared with the documentation
+    description='A Sage package for DD-finite functions',
+    ong_description = readfile("README.txt"), # get the long description from the README
+    # For a Markdown README replace the above line by the following two lines:
+    #  long_description = readfile("README.md"),
+    #  long_description_content_type="text/markdown",
+    url='https://www.dk-compmath.jku.at/Members/antonio/sage-package-dd_functions',
     author = "Antonio Jimenez-Pastor",
     author_email = "antonio.jimenez-pastor@dk-compmath.jku.at",
-    license = "GPL",
-    packages = ["ajpastor", "ajpastor.dd_functions", "ajpastor.operator", "ajpastor.lazy", "ajpastor.misc", "ajpastor.tests"]
-    )
+    license = "GPLv3+", # See LICENCE file
+    classifiers=[
+      # How mature is this project? Common values are
+      #   3 - Alpha
+      #   4 - Beta
+      #   5 - Production/Stable
+      'Development Status :: 3 - Alpa',
+      'Intended Audience :: Science/Research',
+      'Topic :: Software Development :: Build Tools',
+      'Topic :: Scientific/Engineering :: Mathematics',
+      'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+      'Programming Language :: Python :: 2.7',
+    ], # classifiers list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords = "holonomic SageMath dfinite",
+    packages = ["ajpastor"],
+    cmdclass = {'test': SageTest}, # adding a special setup command for tests
+    setup_requires   = ['sage-package'],
+    install_requires = ['sage-package', 'sphinx'],
+)
     
