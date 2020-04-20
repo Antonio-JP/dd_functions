7a82f5c8c4f3845d355af433f9a3b94211ac9e12
Antonio_JP <kethewal@gmail.com>
2020-04-20 14:03:00 +0200

    Adapted conf.py to Python 3
    Changed some calls to method "unicode" in the configuration file
    "conf.py" for creating the documentation of the packages. Now all strings
    are unicode so thee is no need to use this notation anymore.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -232,7 +232,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', package_name + '.tex', u'Documentation of ' + unicode(package_name),
+  ('index', package_name + '.tex', u'Documentation of ' + package_name,
    authors, 'manual'),
 ]
 
@@ -262,7 +262,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', package_name, unicode(package_name) + u" documentation",
+    ('index', package_name, package_name + u" documentation",
      [authors], 1)
 ]
 
@@ -276,7 +276,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', package_name, unicode(package_name) + u" documentation",
+  ('index', package_name, package_name + u" documentation",
    authors, package_name, project,
    'Miscellaneous'),
 ]
