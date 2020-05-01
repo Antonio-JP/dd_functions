57a2c6eb2082e7667cc1a8a8202c157c3ac08106
Antonio_JP <kethewal@gmail.com>
2020-05-01 21:10:18 +0200

    Added file with exception for dd_functions
    Added a file with more Exceptions and Errors for the
    package ajpastor.dd_functions.
    
    Added the documentation files also for autogenerate it with Sphinx.

---
+++ ajpastor/dd_functions/exceptions.py
@@ -0,0 +1,94 @@
+r"""
+    Python file for exceptions in the package dd_functions
+
+    This file contains all the different exceptions that we need in the computations
+    with DDFunctions and similar structures. These exceptions allows a better control
+    of the errors through the package.
+
+    These exceptions are available publicaly, but need to be imported explicitly (i.e.,
+    these exception are not directly imported with the package ``ajpastor.dd_function``)
+
+    EXAMPLES::
+
+        sage: from ajpastor.dd_functions.exceptions import *
+        sage: raise InitValueError("error with initial values")
+        Traceback (most recent call last):
+        ...
+        InitValueError: error with initial values
+        sage: raise ZeroValueRequired()
+        Traceback (most recent call last):
+        ...
+        ZeroValueRequired: required a zero value
+
+    AUTHORS:
+
+        - Antonio Jimenez-Pastor (2020-05-01): initial version
+
+"""
+
+# ****************************************************************************
+#  Copyright (C) 2019 Antonio Jimenez-Pastor <ajpastor@risc.uni-linz.ac.at>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#                  https://www.gnu.org/licenses/
+# ****************************************************************************
+
+class InitValueError(ValueError):
+    r'''
+        Exception due to an error in initial values of a DDFunction
+
+        This error is provoke because a DDFunction needs a different type
+        of initial condition that the one given. This could be because
+        the user is imposing an impossible value for a derivative, an 
+        unexpected error on computations or composing two DDFunctions with
+        incompatible initial conditions.
+
+        TODO:
+            * Look and substitue for this error
+    '''
+    pass
+
+class ZeroValueRequired(InitValueError):
+    r'''
+        Particular exception for requiring a zero initial condition.
+
+        This error is a particular case of :class:`InitValueError` (see its documentation
+        for further information) where the problem is that `0` was required as the
+        first initial condition.
+
+        Two different type of errors can be build: with one or two arguments. The 
+        message is adapted to the type of error.
+
+        EXAMPLE::
+
+            sage: from ajpastor.dd_functions.exceptions import ZeroValueRequired
+            sage: raise ZeroValueRequired()
+            Traceback (most recent call last):
+            ...
+            ZeroValueRequired: required a zero value
+            sage: raise ZeroValueRequired("f(x)")
+            Traceback (most recent call last):
+            ...
+            ZeroValueRequired: required a zero value for f(x)
+            sage: raise ZeroValueRequired("f(x)", "cos(f(x))")
+            Traceback (most recent call last):
+            ...
+            ZeroValueRequired: required a zero value for f(x) in cos(f(x))
+            sage: raise ZeroValueRequired("f(x)", "cos(f(x))", "useless argument")
+            Traceback (most recent call last):
+            ...
+            ZeroValueRequired: required a zero value for f(x) in cos(f(x))
+
+        TODO:
+            * Look and substitue for this error
+    '''
+    def __init__(self, *args):
+        if(len(args) == 0):
+            super(ZeroValueRequired, self).__init__("required a zero value")
+        elif(len(args) == 1):
+            super(ZeroValueRequired, self).__init__("required a zero value for %s" %args[0])
+        else:
+            super(ZeroValueRequired, self).__init__("required a zero value for %s in %s" %(args[0], args[1]))
