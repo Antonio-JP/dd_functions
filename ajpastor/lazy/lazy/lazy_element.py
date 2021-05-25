576af5db2e2521622c570f470f757037fed1781e
Antonio-JP <kethewal@gmail.com>
2021-05-25 17:31:12 +0200

    Improved version of the lazy elements

--- ajpastor/lazy/lazy/lazy_element.py
+++ ajpastor/lazy/lazy/lazy_element.py
@@ -1,4 +1,305 @@
+r"""
+File for the element structure of Lazy rings
 
+This file contains all the element structures for Lazy rings. See documentation
+of :mod:`~ajpastor.lazy.lazy.lazy_ring` for further information.
 
-class LazyElement():
-    pass
+AUTHORS:
+
+    - Antonio Jimenez-Pastor (2012-05-25): initial version
+
+"""
+
+# ****************************************************************************
+#  Copyright (C) 2021 Antonio Jimenez-Pastor <ajpastor@risc.uni-linz.ac.at>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#                  https://www.gnu.org/licenses/
+# ****************************************************************************
+
+from sage.structure.element import CommutativeRingElement # pylint: disable=no-name-in-module
+
+def is_LazyElement(element):
+    r'''
+        Method to decide whether or not an element is a polynomial with infinite variables.
+
+        This is a call to ``isinstance`` with the two main element classes of Infinite Polynomial
+        Rings.
+    '''
+    return isinstance(element, LazyElement)
+
+class LazyElement(CommutativeRingElement):
+    r'''
+        Class for elements in a :class:`~ajpastor.lazy.lazy.lazy_ring.LazyRing`.
+
+        This class is used to represent any object on a :class:`~ajpastor.lazy.lazy.lazy_ring.LazyRing`.
+        These objects have the particularity that are splitted in two parts: a lazy polynomial and 
+        a real element.
+
+        Computations are performed lazily using the polynomials and the computation of the real 
+        object is only done when needed. The method :func:`real` will compute such value and
+        all the methods starting with ``verified_*`` will do the usual computation but guaranteeing
+        the real value of the output is computed.
+
+        INPUT:
+
+        * ``parent``: the parent structure (a :class:`~ajpastor.lazy.lazy.lazy_ring.LazyRing`) where ``self``
+          will be included.
+        * ``real``: the real element (see :func:`~ajpastor.lazy.lazy.lazy_ring.LazyRing.ring`)
+        * ``polynomial``: the lazy representation of ``self``.
+    '''
+    def __init__(self, parent, real, polynomial):
+        from .lazy_ring import LazyRing
+        if(not isinstance(parent, LazyRing)):
+            raise TypeError("The parent must be a LazyRing")
+        if(not polynomial in parent.poly_ring):
+            raise TypeError("The polynomial representation must be in the corresponding polynomial ring")
+
+        super().__init__(parent)
+        self.__real = real
+        self.__polynomial = polynomial
+
+    @property
+    def real(self):
+        r'''
+            Attribute for the real element represented by ``self``.
+        '''
+        if(not self.computed()):
+            self.__real = self.polynomial(**self.parent().map_of_vars)
+        return self.__real
+
+    def computed(self):
+        r'''
+            Method that checks if the real values has been computed.
+        '''
+        return not(self.__real is None)
+
+    polynomial = property(lambda self: self.__polynomial) #: Attribute with the polynomial representation of ``self``.
+
+    ## Arithmetic methods
+    def _add_(self, x):
+        return LazyElement(self.parent(), None, self.polynomial + x.polynomial)
+    def _sub_(self, x):
+        return LazyElement(self.parent(), None, self.polynomial - x.polynomial)
+    def _mul_(self, x):
+        print("here")
+        return LazyElement(self.parent(), None, self.polynomial * x.polynomial)
+    def _rmul_(self, x):
+        return LazyElement(self.parent(), None, self.polynomial._rmul_(x.polynomial))
+    def _lmul_(self, x):
+        return LazyElement(self.parent(), None, self.polynomial._lmul_(x.polynomial))
+    def __pow__(self, n):
+        return LazyElement(self.parent(), None, self.polynomial**n)
+
+    ## Simplicity methods
+    def reduce(self, relations):
+        r'''
+            Method that reduces the current polynomial representation of a Lazy element
+
+            This method changes the internal information about this lazy element (see 
+            :func:`polynomial`). This could change the nature of the element, making it a
+            very simple element.
+
+            This method never discovers a new relation, and no computation in the real ring are
+            performed.
+
+            INPUT:
+
+            * ``relations``: list of relations (i.e., polynomials that are equal to zero in the real
+              ring).
+            
+            OUTPUT:
+
+            The method returns ``self``.
+
+            TODO: add examples and tests
+        '''
+        R = self.parent().poly_ring._P # polynomial ring where everything will be contained
+
+        relations = [R(el) for el in relations]# We convert all the element in the relation to R
+        poly = R(self.polynomial) # we convert the polynomial representation of self into R
+
+        result = poly.reduce(relations) # reducing via the reduce method for polynomials
+        
+        self.__polynomial = self.parent().poly_ring(result) # changing the stored value
+        return self
+
+    ## Other operations
+    def gcd(self, other):
+        r'''
+            Method that comptues the greatest common divisor of two elements.
+
+            This method computes the GCD of ``self`` with another object of ``self.parent()``
+            using the information within the lazy representation (see :func:`polynomial`).
+
+            This method never discovers a new relation, and no computations in the real ring are
+            performed. Moreover, the result may change after repetition, since the lazy 
+            representation may change through time. 
+        '''
+        other = self.parent()(other)
+        return LazyElement(self.parent(), None, self.polynomial.gcd(other.polynomial))
+
+    def derivative(self, times=1):
+        raise NotImplementedError()
+
+    ## Equality methods
+    def is_zero(self):
+        r'''
+            Method to check is an object is zero.
+
+            This method checks if the current lazy representation of ``self`` is zero.
+            This method does not perform any operation on the real ring (so it is a fast
+            method) but it could change the output through time.
+
+            For completely cheking if ``self`` is zero, use method :func:`verify_zero`.
+
+            OUTPUT:
+
+            ``True`` if the lazy representation of ``self`` is zero.
+
+            TODO: add tests and examples
+        '''
+        ## We first simplify the element (in case new relations are found)
+        self.parent().simplify(self)
+
+        return self.polynomial == 0
+
+    def verify_zero(self):
+        r'''
+            Method to check is an object is zero.
+
+            This method checks if the real representation of ``self`` is zero.
+            This method **performs** operations on the real ring. For a faster (but incomplete)
+            checking, use method :func:`is_zero`.
+
+            OUTPUT:
+
+            ``True`` if ``self`` is zero.
+
+            TODO: add tests and examples
+        '''
+        if(not self.computed()): # if it is not computed, we use the lazy information first
+            if(self.is_zero()): # we see if the polynomial gets to zero
+                return True
+        ## Otherwise we use the real value of ``self``.
+        result = self.real == 0
+        if(result): # Updating the relations
+            self.parent().add_relations(self)
+            self.__real = self.parent().ring.zero()
+            self.__polynomial = self.parent().poly_ring.zero()
+
+        return result
+    
+    def is_one(self):
+        r'''
+            Method to check is an object is one.
+
+            This method checks if the current lazy representation of ``self`` is one.
+            This method does not perform any operation on the real ring (so it is a fast
+            method) but it could change the output through time.
+
+            For completely cheking if ``self`` is one, use method :func:`verify_one`.
+
+            OUTPUT:
+
+            ``True`` if the lazy representation of ``self`` is one.
+
+            TODO: add tests and examples
+        '''
+        ## We first simplify the element (in case new relations are found)
+        self.parent().simplify(self)
+
+        return self.polynomial == 1
+
+    def verify_one(self):
+        r'''
+            Method to check is an object is one.
+
+            This method checks if the real representation of ``self`` is one.
+            This method **performs** operations on the real ring. For a faster (but incomplete)
+            checking, use method :func:`is_one`.
+
+            OUTPUT:
+
+            ``True`` if ``self`` is one.
+
+            TODO: add tests and examples
+        '''
+        if(not self.computed()): # if it is not computed, we use the lazy information first
+            if(self.is_one()): # we see if the polynomial gets to zero
+                return True
+        ## Otherwise we use the real value of ``self``.
+        result = self.real == 1
+        if(result): # Updating the relations
+            self.parent().add_relations(self-1)
+            self.__real = self.parent().ring.one()
+            self.__polynomial = self.parent().poly_ring.one()
+
+        return result
+
+    def equals(self, other):
+        r'''
+            Method to check equality between objects.
+
+            This method checks if the current lazy representation of ``self`` and ``other`` 
+            are the same. This method does not perform any operation on the real ring (so it is a fast
+            method) but it could change the output through time.
+
+            For completely cheking if ``self`` is ``other``, use method :func:`verify_equals`.
+
+            INPUT: 
+
+            * ``other``: an object we will check if it is equals to ``self``.
+
+            OUTPUT:
+
+            ``True`` is the current lazy representation of ``self`` coincides with the 
+            lazy representation of ``other``.
+
+            TODO: add examples and tests.
+        '''
+        try:
+            other = self.parent()(other)
+            
+            return (self-other).is_zero()
+        except: # other can not be casted into ``self.parent()``
+            return False
+
+    def verify_equals(self, other):
+        r'''
+            Method to check equality between objects.
+
+            This method checks if ``self`` and ``other`` are the same object.
+            This method performs operations on the real ring that may be slow. 
+            For a faster (but incomplete) checking of equality, use the method
+            :func:`equals`.
+
+            INPUT: 
+
+            * ``other``: an object we will check if it is equals to ``self``.
+
+            OUTPUT:
+
+            ``True`` is ``self`` coincides with ``other``.
+
+            TODO: add examples and tests.
+        '''
+        try:
+            other = self.parent()(other)
+            
+            return (self-other).verify_zero()
+        except: # other can not be casted into ``self.parent()``
+            return False
+
+    ## Representation methods
+    def _repr_(self):
+        if(not self.computed()):
+            return "[%s]" %repr(self.polynomial)
+        else:
+            return "(%s [%s])" %(self.real, self.polynomial)
+
+    def _latex_(self):
+        return r"\mathcal{Lazy}(" + (r"%s \rightarrow" %self.real if self.computed() else "") + r"%s" %self.polynomial
--- ajpastor/lazy/lazy/lazy_ring.py
+++ ajpastor/lazy/lazy/lazy_ring.py
@@ -23,7 +23,10 @@ r'''
 
 from .lazy_element import LazyElement
 
-from sage.all import CommutativeRing, CommutativeRings, IntegralDomains, parent, Matrix, vector
+from sage.all import CommutativeRing, CommutativeRings, IntegralDomains, parent, Matrix, vector, ideal, latex
+from sage.categories.all import Morphism
+from sage.categories.pushout import pushout, ConstructionFunctor
+from sage.rings.fraction_field import is_FractionField
 from sage.rings.polynomial.infinite_polynomial_ring import InfinitePolynomialRing
 from sage.structure.element import is_Matrix, is_Vector # pylint: disable=no-name-in-module
 from sage.structure.unique_representation import UniqueRepresentation
@@ -51,6 +54,14 @@ class LazyRing(CommutativeRing, UniqueRepresentation):
 
     Element = LazyElement
 
+    @staticmethod
+    def __classcall__(cls, ring):
+        ## We avoid iterative constructions over lazy rings
+        if(isinstance(ring, LazyRing)):
+            ring = ring.ring
+
+        return super().__classcall__(cls, ring)
+
     def __init__(self, ring):
         if(not ring in _CommutativeRings):
             raise TypeError("LazyRing must wrap an actual commutative ring")
@@ -76,10 +87,21 @@ class LazyRing(CommutativeRing, UniqueRepresentation):
         self.__var_to_real = {} # See method map_of_vars
         self.__real_to_var = {} # See method map_of_elements
         self.__used = 0 # Attribute for counting the number of used variables
+        self.__relations = [] # Attribute for generators of known relations
+
+        ## Setting new conversions to simpler structures
+        current = self.ring
+        morph = LazyRing_Base(self, current)
+        current.register_conversion(morph)
+        while(not(current.base() == current)):
+            current = current.base()
+            morph = LazyRing_Base(self, current)
+            current.register_conversion(morph)
 
     ring = property(lambda self: self.__ring) #: Immutable attribute for the real ring
     poly_ring = property(lambda self : self.__poly) #: Immutable attribute for the insider polynomial ring
     poly_field = property(lambda self : self.poly_ring.fraction_field()) #: Immutable attribute for the insider polynomial fraction field
+    relations = property(lambda self : self.__relations) #: Immutable attribute for the generators of current relations
 
     def map_of_vars(self):
         r'''
@@ -110,7 +132,7 @@ class LazyRing(CommutativeRing, UniqueRepresentation):
 
             TODO: add examples and tests
         '''
-        return self.__var_to_real
+        return self.__real_to_var
 
     ## Inclusion methods
     def __contains__(self, element):
@@ -158,15 +180,11 @@ class LazyRing(CommutativeRing, UniqueRepresentation):
 
             TODO: add examples and tests
         '''
-        ## Cases accepted by self.__contains__
-        if(parent(element) is self):
-            return element
-        elif(element in self.ring):
-            return self._create_from_real(self.ring(element))
-        elif(element in self.poly_ring):
-            return self._create_from_poly(self.poly_ring(element))
+        ## Cases of__contains__
+        if(element in self):
+            return super().__call__(element)
         ## Cases of fractions
-        elif(self.ring in _IntegralDomains):
+        if(self.ring in _IntegralDomains):
             if(element in self.ring.fraction_field()):
                 n = self(self.ring(element.numerator()))
                 d = self(self.ring(element.denominator()))
@@ -176,19 +194,42 @@ class LazyRing(CommutativeRing, UniqueRepresentation):
                 d = self(self.poly_ring(element.denominator()))
                 return n/d
         ## Cases of other structures
-        elif(is_Matrix(element)):
+        if(is_Matrix(element)):
             return Matrix(self, [[self(el) for el in row] for row in element])
-        elif(is_Vector(element)):
+        if(is_Vector(element)):
             return vector(self, [self(el) for el in element])
-        elif(isinstance(element, list)):
+        if(isinstance(element, list)):
             return [self(el) for el in element]
-        elif(isinstance(element, set)):
+        if(isinstance(element, set)):
             return set([self(el) for el in element])
-        elif(isinstance(element, tuple)):
+        if(isinstance(element, tuple)):
             return tuple([self(el) for el in element])
-        else:
-            raise TypeError("Wrong type: Impossible to get lazy element from (%s)" %(element))
 
+        raise TypeError("Error casting an element (%s) into the Lazy framework %s" %(element, self))
+
+    def _element_constructor_(self, element):
+        r'''
+            Method to create an element in ``self`` from an object that is *in* ``self``.
+
+            This method implements the way Sage construct an element. This can be use in general
+            for checking if something is in ``self`` or, more precisely, to build an 
+            object for any of the possible inputs allowed.
+
+            INPUT:
+
+            * ``element``: data from which construct a new object in ``self``.
+
+            TODO: add examples and tests
+        '''
+        print("_element_constructor_")
+        if(parent(element) is self):
+            return element
+        elif(element in self.ring):
+            return self._create_from_real(self.ring(element))
+        elif(element in self.poly_ring):
+            return self._create_from_poly(self.poly_ring(element))
+        raise TypeError("Type not recognized to create an element")
+            
     def _create_from_real(self, element):
         r'''
             Method that create a lazy element from a real element
@@ -242,4 +283,292 @@ class LazyRing(CommutativeRing, UniqueRepresentation):
         '''  
         return self.Element(self, self.map_of_vars().get(str(element), None), element)
 
-## GO ON WITH THE RELATIONS AND SIMPLIFY METHODS
+    ## Coercion methods
+    def _has_coerce_map_from(self, S):
+        r'''
+            Standard implementation for ``_has_coerce_map_from``
+        '''
+        coer =  self._coerce_map_from_(S)
+        return (not(coer is False) and not(coer is None))
+
+    def _coerce_map_from_(self, S):
+        if(self.ring._coerce_map_from_(S)):
+            return True
+        return None
+
+    def construction(self):
+        r'''
+            Returns a functor that builds the :class:`Lazyring`.
+
+            Method that construct a functor `F` and an commutative ring `R` such that ``self == F(R)``. This method allows the 
+            coerce system in Sage to handle :class:`LazyElement` and :class:`LazyRing` appropriately.
+
+            OUTPUT:
+
+            This method returns a tuple `(F,R)` where `F` is a functor such that ``self == F(R)``.
+
+            EXAMPLES::
+
+                sage: from ajpastor.lazy.lazy.lazy_ring import *
+                sage: A = LazyRing(QQ)
+                sage: F,R = A.construction()
+                sage: F == LazyRingFunctor()
+                True
+                sage: R == QQ
+                True
+                sage: B = LazyRing(A)
+                sage: F,R = B.construction()
+                sage: F == DDRingFunctor()
+                True
+                sage: R == QQ
+                True
+        '''
+        return (LazyRingFunctor(), self.ring)
+
+    def _pushout_(self, S):
+        r'''
+            Mathod to compute the pushout of ``self`` with ``S``.
+
+            This method computes a :class:`LazyRing` that contains both ``self`` and ``S`` or returns ``None``.
+            The idea behind this pushout is that:
+
+            * If `R \subset S`, then the lazy ring of `R` is contained in the lazy ring of `S`.
+            * For any ring `R`, it is contained in the lazy ring of `R`.
+
+            INPUT:
+
+            * ``S``: the other structure to compare.
+
+            OUTPUT:
+
+            ``None`` if there is no valid pushout from ``self`` or a :class:`LazyRing` contining both ``self`` and ``S``.
+
+            TODO: add examples and tests.
+        '''
+        print("Here")
+        try:
+            if(isinstance(S, LazyRing)):
+                R = pushout(self.ring, S.ring)
+            else:
+                R = pushout(self.ring, S)
+        except: # any error implies there is not known pushout from ``self``.
+            return None
+        
+        return LazyRing(R)
+
+    def gens(self):
+        return tuple(self(el) for el in self.ring.gens())
+
+    def ngens(self):
+        return len(self.gens())
+
+    ## Relations and simplification methods
+    def add_relations(self, relations):
+        r'''
+            Method that add new relations to a lazy system.
+
+            This method takes a series of elements in ``self`` (which is checked using a
+            call to ``self.simplify(relations)``) and then adds all the polynomial representations
+            of the elements to the computed relations.
+
+            This method assumes that the elements are indeed zero. Otherwise the 
+            behavior of this method is not guaranteed.
+
+            INPUT:
+
+            * ``relations``: element in ``self`` or iterable of elements in ``self``. 
+
+            OUTPUT:
+
+            The final generators of the relations. 
+
+            TODO: add examples and tests
+        '''
+        relations = self.simplify(relations)
+        R = self.poly_ring._P # biggest polynomial ring found so far
+        self.__relations = [R(el) for el in self.relations]
+        if(relations in self): # just one relation
+            if(relations.polynomial != 0):
+                self.__relations += [R(relations.polynomial.polynomial())]
+        elif(is_Vector(relations) or type(relations) in (tuple, list, set)):
+            self.__relations += [R(el.polynomial.polynomial()) for el in relations if el.polynomial != 0] 
+        if(len(self.relations) > 0):
+            self.__relations = ideal(self._relations).groebner_basis()
+
+        return self.relations
+
+    def simplify(self, element):
+        r'''
+            Method to simplify an element involving the lazy system.
+
+            This method simplifies a structure that contains lazy elements using the
+            discovered relations (see :func:`relations`) in the lazy ring. This 
+            will imply casting the element to a polynomial form, and substitute that
+            representation for the reduced version.
+
+            This method allows to simplify a wide variety of structures (as it did 
+            the casting system into a lazy ring). Namely:
+
+            * Iterable objects: such as ``list``, ``tuple`` or ``set``.
+            * Vectors: see :func:`~sage.structure.element.is_Vector`.
+            * Matrices: see :func:`~sage.structure.element.is_Matrix`.
+            * Fractions: this method will also work with fractions of 
+              elements in :func:`ring`.
+
+            INPUT:
+
+            * ``element``: the element to be simplified.
+
+            OUTPUT:
+
+            An object with the same structure as ``element`` but with all the
+            lazy elements simplified.
+
+            TODO: add examples and tests
+        '''
+        element = self(element)
+        ## Element in the lazy system
+        if(element in self):
+            element.reduce(self.relations)
+        ## Cases of fractions
+        elif(self.ring in _IntegralDomains):
+            if(is_FractionField(parent(element))):
+                self.simplify(element.numerator())
+                self.simplify(element.denominator())
+                return element
+        ## Cases of other structures
+        elif(is_Matrix(element)):
+            for row in element:
+                for el in row:
+                    self.simplify(el)
+        elif(is_Vector(element)):
+            for el in element:
+                self.simplify(el)
+        elif(isinstance(element, list)):
+            for el in element:
+                self.simplify(el)
+        elif(isinstance(element, set)):
+            for el in element:
+                self.simplify(el)
+        elif(isinstance(element, tuple)):
+            for el in element:
+                self.simplify(el)
+        else:
+            raise TypeError("Wrong type: Impossible to get lazy element from (%s)" %(element))
+
+        return element
+
+    ## Representation methods
+    def _repr_(self):
+        return "Lazy ring over [%s]" %(self.ring)
+
+    def _latex_(self):
+        return r"\mathcal{Lazy}(%s)" %(latex(self.ring))
+
+    ## Element methods
+    def one(self):
+        r'''
+            Return the one element in ``self``.
+
+            EXAMPLES::
+
+                sage: from ajpastor.lazy.lazy.lazy_ring import *
+                sage: R = LazyRing(QQ['x'])
+                sage: R.one()
+                1
+        '''
+        return self(1)
+    
+    def zero(self):
+        r'''
+            Return the zero element in ``self``.
+
+            EXAMPLES::
+
+                sage: from ajpastor.lazy.lazy.lazy_ring import *
+                sage: R = LazyRing(QQ['x'])
+                sage: R.zero()
+                0
+        '''
+        return self(0)
+
+    def random_element(self,*args,**kwds):
+        r'''
+            Creates a randome element in this ring.
+
+            This method creates a random element in the base infinite polynomial ring and 
+            cast it into an element of ``self``.
+        '''
+        try:
+            p = self.ring.random_element(*args,**kwds)
+        except AttributeError:
+            p = self.ring.one()
+        return self(p)
+
+    ## Ring methods
+    def is_integral_domain(self):
+        return self.ring.is_integral_domain()
+
+    def is_noetherian(self):
+        return self.ring.is_noetherian()
+
+    def is_field(self):
+        return self.ring.is_field()
+
+class LazyRingFunctor (ConstructionFunctor):
+    r'''
+        Class representing Functor for creating :class:`LazyRing`.
+
+        This functor maps commutative rings into other structures that 
+        implements the lazy computations as intended. This functor does
+        not require any extra argument since it will create the corresponding
+        :class:`LazyRing` from any ring.
+
+        EXAMPLES::
+
+            sage: from ajpastor.lazy.lazy.lazy_ring import *
+            sage: R = LazyRing(QQ['x'])
+            sage: F, S = R.construction()
+            sage: F(S) is R
+            True
+            sage: F
+            LazyRing(*)
+    '''
+    def __init__(self):
+        self.rank = 12
+        super().__init__(_CommutativeRings,_CommutativeRings)
+        
+    ### Methods to implement
+    def _coerce_into_domain(self, x):
+        if(x not in self.domain()):
+            raise TypeError("The object [%s] is not an element of [%s]" %(x, self.domain()))
+        return x
+        
+    def _apply_functor(self, x):
+        if(isinstance(x, LazyRing)):
+            return x
+        return LazyRing(x)
+        
+    def _repr_(self):
+        return "LazyRing(*)" %(str(self.__variables))
+        
+    def __eq__(self, other):
+        return other.__class__ == self.__class__
+
+    def merge(self, other):
+        scls = self.__class__; ocls = other.__class__
+        if(issubclass(scls, ocls) and not (issubclass(ocls, scls))):
+            return other
+        elif(issubclass(ocls, scls)):
+            return self
+        return None
+
+    def variables(self):
+        return self.__variables
+
+class LazyRing_Base (Morphism):
+    def __init__(self, domain, codomain):
+        super().__init__(domain, codomain)
+        
+    def _call_(self, p):
+        return self.codomain()(p.real)
