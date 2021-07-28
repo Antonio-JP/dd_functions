b0caf62e0fe5fd69bb22819e31612829069bc3ae
Antonio-JP <kethewal@gmail.com>
2021-07-28 09:22:55 +0200

    Rebuilt documentation

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.9.20210218.1157',
+    VERSION: '0.9.20210726.1108.8alpha2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
