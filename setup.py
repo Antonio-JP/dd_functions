d2c976613adb3aa57d43bf9d04b61fb7c879a749
Antonio Jimenez-Pastor <kethewal@gmail.com>
2025-06-04 13:00:40 +0200

    ♻️  Prepared demo for presentation

--- setup.py
+++ setup.py
@@ -17,9 +17,9 @@ setup(
     # For a Markdown README replace the above line by the following two lines:
     long_description = readfile("README.md"),
     long_description_content_type="text/markdown",
-    url='https://www.dk-compmath.jku.at/Members/antonio/sage-package-dd_functions',
+    url='https://github.com/Antonio-JP/dd_functions',
     author = "Antonio Jimenez-Pastor",
-    author_email = "antonio.jimenez-pastor@dk-compmath.jku.at",
+    author_email = "antonio.jimenezp@upm.es",
     license = "GPLv3+", # See LICENCE file
     classifiers=[
       # How mature is this project? Common values are
@@ -31,7 +31,7 @@ setup(
       'Topic :: Software Development :: Build Tools',
       'Topic :: Scientific/Engineering :: Mathematics',
       'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-      'Programming Language :: Python :: 3.8.5',
+      'Programming Language :: Python :: 3.10.12',
     ], # classifiers list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
     keywords = "holonomic SageMath dfinite",
     packages = ["ajpastor",
@@ -39,7 +39,7 @@ setup(
         "ajpastor.operator",
         "ajpastor.lazy",
         "ajpastor.misc"],
-    setup_requires   = [],
-    install_requires = ['ore_algebra @ git+https://github.com/mkauers/ore_algebra.git','sphinx'],
+    setup_requires   = ['sphinx'],
+    install_requires = ['ore_algebra @ git+https://github.com/mkauers/ore_algebra.git'],
 )
     
